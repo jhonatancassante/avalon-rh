26987cc4404a71fab29b3b8894df97042df80e3c
Jhonatan Cassante de Brito <jhonatan.cassante@live.com>
2025-02-12 15:18:47 -0300

    feat: add LoadingIndicator component and update Home page for loading state

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,25 @@
-import { Button } from "./_components/ui/button";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGoogle } from "@fortawesome/free-brands-svg-icons";
+"use client";
+
+import { useSession } from "next-auth/react";
+import LoginCard from "./_components/login-card";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import LoadingIndicator from "./_components/loading-indicator";
 
 export default function Home() {
+    const session = useSession();
+    const router = useRouter();
+    const isLoading = session.status === "loading" || session.data?.user;
+
+    useEffect(() => {
+        if (session.data?.user) {
+            router.push("/pages/home");
+        }
+    }, [router, session.data]);
+
     return (
-        <div>
-            <Button>Eita</Button>
-            <FontAwesomeIcon icon={faGoogle} style={{ fontSize: "10px" }} />
+        <div className="flex min-h-screen items-center justify-center bg-primary-foreground">
+            {isLoading ? <LoadingIndicator /> : <LoginCard />}
         </div>
     );
 }
