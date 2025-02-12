0081af4e46979c9beba1a2fffdb87185e404978a
Jhonatan Cassante de Brito <jhonatan.cassante@live.com>
2025-02-12 16:26:46 -0300

    feat: add remote image patterns for Google services in Next.js config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    images: {
+        remotePatterns: [
+            {
+                hostname: "lh3.googleusercontent.com",
+            },
+            { hostname: "drive.google.com" },
+        ],
+    },
 };
 
 export default nextConfig;
