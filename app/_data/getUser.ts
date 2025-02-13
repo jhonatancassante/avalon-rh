b0f7f360de57906f164f47ee0eade611065e5e68
Jhonatan Cassante de Brito <jhonatan.cassante@live.com>
2025-02-13 09:39:26 -0300

    feat: add server session check to getUser function for authorization

--- app/_data/getUser.ts
+++ app/_data/getUser.ts
@@ -1,6 +1,14 @@
+"use server";
+
+import { getServerSession } from "next-auth";
+import { authOptions } from "../_lib/auth";
 import { db } from "../_lib/prisma";
 
 export const getUser = async (id: string) => {
+    const session = await getServerSession(authOptions);
+
+    if (!session) throw new Error("Unauthorized!");
+
     return await db.user.findUnique({
         where: {
             id: id,
