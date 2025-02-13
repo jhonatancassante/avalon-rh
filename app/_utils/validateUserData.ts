237a2ae09740933a1c9050e88860c94f126a0337
Jhonatan Cassante de Brito <jhonatan.cassante@live.com>
2025-02-13 18:36:47 -0300

    feat: update validateUserData to use UpdateUser type and format birthdate

--- app/_utils/validateUserData.ts
+++ app/_utils/validateUserData.ts
@@ -1,10 +1,21 @@
 import { z } from "zod";
 import { formSchema } from "../_schemas/formSchema";
-import { User } from "@prisma/client";
+import UpdateUser from "../_types/UpdateUser";
+import { format } from "date-fns";
+import { ptBR } from "date-fns/locale";
 
-export const validateUserData = (data: User) => {
+export const validateUserData = (data: UpdateUser) => {
     try {
-        formSchema.parse(data);
+        const dataToValidade =
+            typeof data.birthdate === "string"
+                ? data
+                : {
+                      ...data,
+                      birthdate: format(data.birthdate, "yyyy/MM/dd", {
+                          locale: ptBR,
+                      }),
+                  };
+        formSchema.parse(dataToValidade);
         return { isValid: true, errors: null };
     } catch (error) {
         if (error instanceof z.ZodError) {
