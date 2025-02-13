d8c98226ea95c90559a0fa77bd12cda643e76033
Jhonatan Cassante de Brito <jhonatan.cassante@live.com>
2025-02-13 06:19:31 -0300

    feat: handle user retrieval errors and redirect to error page

--- app/pages/user/edit/[id]/page.tsx
+++ app/pages/user/edit/[id]/page.tsx
@@ -6,49 +6,55 @@ import {
 } from "@/app/_components/ui/card";
 import UserEditForm from "@/app/_components/user-edit-form";
 import { getUser } from "@/app/_data/getUser";
+import { redirect } from "next/navigation";
 
 interface EditUserPageProps {
     params: Promise<{ id: string }>;
 }
 
 const EditUserPage = async ({ params }: EditUserPageProps) => {
-    const { id } = await params;
-    const user = await getUser(id);
+    try {
+        const { id } = await params;
+        const user = await getUser(id);
 
-    if (!user) {
-        return <div>User not found</div>;
+        if (!user) {
+            throw new Error("User not found!");
+        }
+
+        return (
+            <main className="flex justify-center p-5 lg:px-28">
+                <Card className="w-full max-w-2xl">
+                    <CardHeader>
+                        <CardTitle className="text-2xl font-bold">
+                            Editar Perfil
+                        </CardTitle>
+                    </CardHeader>
+                    <CardContent>
+                        <UserEditForm
+                            id={user.id ?? ""}
+                            name={user.name ?? ""}
+                            cpf={user.cpf ?? ""}
+                            completeName={user.completeName ?? ""}
+                            email={user.email ?? null}
+                            secondaryEmail={user.secondaryEmail ?? ""}
+                            birthdate={user.birthdate ?? null}
+                            role={user.role ?? ""}
+                            createdAt={user.createdAt ?? null}
+                            updatedAt={user.updatedAt ?? null}
+                            image={user.image ?? ""}
+                            photoUrl={user.photoUrl ?? ""}
+                            emailVerified={user.emailVerified ?? null}
+                            isComplete={user.isComplete ?? false}
+                            isDeleted={user.isDeleted ?? false}
+                        />
+                    </CardContent>
+                </Card>
+            </main>
+        );
+    } catch (error) {
+        console.error(error);
+        redirect("/pages/errors/500");
     }
-
-    return (
-        <main className="flex justify-center p-5 lg:px-28">
-            <Card className="w-full max-w-2xl">
-                <CardHeader>
-                    <CardTitle className="text-2xl font-bold">
-                        Editar Perfil
-                    </CardTitle>
-                </CardHeader>
-                <CardContent>
-                    <UserEditForm
-                        id={user.id || ""}
-                        name={user.name ?? null}
-                        cpf={user.cpf ?? null}
-                        completeName={user.completeName || null}
-                        email={user.email ?? null}
-                        secondaryEmail={user.secondaryEmail || null}
-                        birthdate={user.birthdate || null}
-                        role={user.role || ""}
-                        createdAt={user.createdAt || new Date()}
-                        updatedAt={user.updatedAt || new Date()}
-                        image={user.image ?? null}
-                        photoUrl={user.photoUrl || null}
-                        emailVerified={user.emailVerified || new Date()}
-                        isComplete={user.isComplete || false}
-                        isDeleted={user.isDeleted || false}
-                    />
-                </CardContent>
-            </Card>
-        </main>
-    );
 };
 
 export default EditUserPage;
