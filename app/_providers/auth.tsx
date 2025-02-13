2d84f09ed876b7bcad8a21f5377751685bdb9869
Jhonatan Cassante de Brito <jhonatan.cassante@live.com>
2025-02-13 18:36:28 -0300

    feat: add refetch interval and window focus to SessionProvider

--- app/_providers/auth.tsx
+++ app/_providers/auth.tsx
@@ -4,7 +4,11 @@ import { SessionProvider } from "next-auth/react";
 import { ReactNode } from "react";
 
 const AuthProvider = ({ children }: { children: ReactNode }) => {
-    return <SessionProvider>{children}</SessionProvider>;
+    return (
+        <SessionProvider refetchInterval={5 * 60} refetchOnWindowFocus={true}>
+            {children}
+        </SessionProvider>
+    );
 };
 
 export default AuthProvider;
