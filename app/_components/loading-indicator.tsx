6f8d3e7d36069d4a924c71a4c9640e7b947eac32
Jhonatan Cassante de Brito <jhonatan.cassante@live.com>
2025-02-15 12:12:34 -0300

    feat: update loading indicator to support theme-based styling

--- app/_components/loading-indicator.tsx
+++ app/_components/loading-indicator.tsx
@@ -1,11 +1,17 @@
+import { useTheme } from "next-themes";
 import React from "react";
 
 const LoadingIndicator: React.FC = () => {
+    const { theme } = useTheme();
+
     return (
-        <div className="absolute left-0 top-0 z-10 h-[110vh] w-full bg-primary opacity-45">
-            <div className="flex h-full w-full flex-col items-center justify-center gap-4">
-                <div className="loader h-24 w-24 animate-spin rounded-full border-4 border-t-4 border-dotted border-t-primary"></div>
-                <div className="text-xl font-bold text-background">
+        <div>
+            <div
+                className={`absolute left-0 top-0 z-10 h-[110vh] w-full bg-foreground ${theme === "light" ? "opacity-90" : "opacity-10"}`}
+            ></div>
+            <div className="absolute left-0 top-0 z-20 flex h-[110vh] w-full flex-col items-center justify-center gap-4">
+                <div className="loader h-24 w-24 animate-spin rounded-full border-4 border-t-4 border-dotted border-t-white"></div>
+                <div className="text-xl font-bold text-white">
                     Carregando...
                 </div>
             </div>
