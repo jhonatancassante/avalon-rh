6ef90dac58b582d1e9a507dae5e80ffc0720dc10
Jhonatan Cassante de Brito <jhonatan.cassante@live.com>
2025-02-15 10:22:34 -0300

    fix: remove session and role checks from Admin and Leader pages

--- app/pages/admin/page.tsx
+++ app/pages/admin/page.tsx
@@ -1,26 +1,6 @@
 "use client";
 
-import { Roles } from "@/app/_constants/roles";
-import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
-
 const AdminPage = () => {
-    const { data: session } = useSession();
-    const router = useRouter();
-    const isLogged = session?.user;
-    const isRole = session?.user?.role === Roles.Admin;
-
-    useEffect(() => {
-        if (!isLogged || !isRole) {
-            router.push("/pages/401");
-        }
-    }, [isLogged, isRole, router]);
-
-    if (!isLogged || !isRole) {
-        return null;
-    }
-
     return (
         <div>
             <div className="p-5 lg:px-28">
