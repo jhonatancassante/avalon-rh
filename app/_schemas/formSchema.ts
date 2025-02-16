160e6c5b5b189740da1b6eab36e7bd243bf180cc
Jhonatan Cassante de Brito <jhonatan.cassante@live.com>
2025-02-15 23:36:50 -0300

    feat: add regex validation for first name and last name fields in form schema

--- app/_schemas/formSchema.ts
+++ app/_schemas/formSchema.ts
@@ -7,23 +7,37 @@ import {
     MAX_FILE_SIZE,
 } from "../_constants/photoValidations";
 
+const lettersAndNumbersRegex = /^[A-Za-z0-9\s]+$/;
+
 export const formSchema = z.object({
     cpf: z.string().refine((value) => isValidCPF(value), "CPF inválido!"),
     completeName: z
         .string()
         .trim()
         .min(5, "Nome muito curto! Mínimo 5 caracteres!")
-        .max(100, "Nome muito longo! Máximo 100 caracteres."),
+        .max(100, "Nome muito longo! Máximo 100 caracteres.")
+        .regex(
+            lettersAndNumbersRegex,
+            "Apenas letras e números são permitidos!",
+        ),
     socialName: z
         .string()
         .trim()
         .min(5, "Nome muito curto! Mínimo 5 caracteres!")
-        .max(100, "Nome muito longo! Máximo 100 caracteres."),
+        .max(100, "Nome muito longo! Máximo 100 caracteres.")
+        .regex(
+            lettersAndNumbersRegex,
+            "Apenas letras e números são permitidos!",
+        ),
     nickname: z
         .string()
         .trim()
         .min(2, "Apelido muito curto! Mínimo 2 caracteres!")
-        .max(20, "Apelido muito longo! Máximo 20 caracteres."),
+        .max(20, "Apelido muito longo! Máximo 20 caracteres.")
+        .regex(
+            lettersAndNumbersRegex,
+            "Apenas letras e números são permitidos!",
+        ),
     contactEmail: z.string().email("Email inválido!"),
     phone: z
         .string()
