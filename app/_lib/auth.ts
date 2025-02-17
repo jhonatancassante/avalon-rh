70e93c90c974d1abe5bc9c9d4ee0040ae6e977c6
Jhonatan Cassante de Brito <jhonatan.cassante@live.com>
2025-02-17 20:58:04 -0300

    fix: add user update handling in JWT and session callbacks

--- app/_lib/auth.ts
+++ app/_lib/auth.ts
@@ -3,6 +3,7 @@ import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { db } from "./prisma";
 import { Adapter, AdapterUser } from "next-auth/adapters";
+import { getUserUpdate } from "../_data/getUserUpdate";
 
 declare module "next-auth" {
     interface Session {
@@ -31,25 +32,39 @@ export const authOptions: AuthOptions = {
         strategy: "jwt",
     },
     callbacks: {
-        async jwt({ token, user }) {
+        async jwt({ token, trigger, user }) {
             if (user) {
                 token.userId = user.id;
                 token.role = (user as CustomAdapterUser).role;
                 token.isComplete = (user as CustomAdapterUser).isComplete;
             }
-
+            if (trigger === "update") {
+                const userUpdate = await getUserUpdate(token.userId as string);
+                token.role = userUpdate.role;
+                token.isComplete = userUpdate.isComplete;
+            }
             return token;
         },
-        async session({ session, token }) {
+        async session({ session, trigger, token }) {
             session.user = {
                 ...session.user,
                 id: token.userId as string,
                 role: token.role as string,
                 isComplete: token.isComplete as boolean,
             };
-
+            if (trigger === "update") {
+                session.user = {
+                    ...session.user,
+                    id: token.userId as string,
+                    role: token.role as string,
+                    isComplete: token.isComplete as boolean,
+                };
+            }
             return session;
         },
     },
     secret: process.env.NEXTAUTH_SECRET,
+    pages: {
+        newUser: "/pages/user",
+    },
 };
