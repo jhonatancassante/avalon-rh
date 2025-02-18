7e120a13790bbc3e4ab1404eb38647e2fc8d578a
Jhonatan Cassante de Brito <jhonatan.cassante@live.com>
2025-02-18 16:32:35 -0300

    style: change loading indicator opacity

--- app/_components/loading-indicator.tsx
+++ app/_components/loading-indicator.tsx
@@ -1,11 +1,9 @@
 "use client";
 
-import { useTheme } from "next-themes";
 import React from "react";
 import { useLoading } from "../_contexts/LoadingContext";
 
 const LoadingIndicator: React.FC = () => {
-    const { theme } = useTheme();
     const { isLoading } = useLoading();
 
     if (!isLoading) return null;
@@ -13,7 +11,7 @@ const LoadingIndicator: React.FC = () => {
     return (
         <div>
             <div
-                className={`fixed left-0 top-0 z-10 h-[110vh] w-full bg-foreground ${theme === "light" ? "opacity-90" : "opacity-10"}`}
+                className={`fixed left-0 top-0 z-10 h-[110vh] w-full bg-foreground opacity-80`}
             ></div>
             <div className="fixed left-0 top-0 z-20 flex h-[110vh] w-full flex-col items-center justify-center gap-4">
                 <div className="loader h-24 w-24 animate-spin rounded-full border-4 border-t-4 border-dotted border-background border-r-foreground border-t-foreground"></div>
