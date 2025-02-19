f3a9030997002fe1e22b9cbefaf4ab501a55fc10
Jhonatan Cassante de Brito <jhonatan.cassante@live.com>
2025-02-19 13:46:03 -0300

    fix: changing the scheduling route

--- app/api/cron/updateInscriptionsClose/route.ts
+++ app/api/cron/updateInscriptionsClose/route.ts
@@ -9,9 +9,6 @@ export async function GET(request: Request) {
         return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
     }
 
-    console.log("##########################################");
-    console.log("Rodou o agendamento. Teste bem sucedido! Uhul!");
-
     try {
         const now = new Date();
         const offset = -3; // UTC-3 (São Paulo)
