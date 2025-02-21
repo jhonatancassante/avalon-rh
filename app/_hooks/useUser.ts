80fd7c9b3d7d5bea41dfe7a5fab2efcd3813352b
Jhonatan Cassante de Brito <jhonatan.cassante@live.com>
2025-02-21 20:04:01 -0300

    fix: resolve undefined user ID issue and improve error handling in useUser hook

--- app/_hooks/useUser.ts
+++ app/_hooks/useUser.ts
@@ -8,8 +8,12 @@ export const useUser = () => {
     const [user, setUser] = useState<UserComplete | null>(null);
 
     const fetchUser = useCallback(async () => {
+        if (!session?.user?.id) {
+            throw new Error("User ID is not available.");
+        }
+
         try {
-            const userFound = await getUser(session?.user?.id ?? "");
+            const userFound = await getUser(session?.user?.id);
             setUser(userFound);
         } catch (error) {
             console.error("Failed to fetch user:", error);
