f6c6484187e5c6f872d0ad12e7b203735c52fa0f
Jhonatan Cassante de Brito <jhonatan.cassante@live.com>
2025-02-21 13:08:37 -0300

    feat: added bulk function to open and close registrations, and end event

--- app/_components/data-table/action-buttons.tsx
+++ app/_components/data-table/action-buttons.tsx
@@ -1,19 +1,62 @@
+"use client";
+
 import { Button } from "../ui/button";
-import { Delete, Pencil, Trash2 } from "lucide-react";
+import { Delete, Pencil, PencilOff, Trash2 } from "lucide-react";
 import {
     Tooltip,
     TooltipContent,
     TooltipProvider,
     TooltipTrigger,
 } from "../ui/tooltip";
+import { Row } from "@tanstack/react-table";
+import {
+    updateEventAreInscriptionsOpen,
+    updateEventIsFinished,
+} from "@/app/_actions/updateEvent";
+import { Event } from "@prisma/client";
 
-export function ActionButtons() {
+interface DataTableActionButtonsProps<TData> {
+    selectedRows: Row<TData>[];
+    onActionCompleted: () => Promise<void>;
+}
+
+export function DataTableActionButtons<TData>({
+    selectedRows,
+    onActionCompleted,
+}: Readonly<DataTableActionButtonsProps<TData>>) {
+    const handleInscriptionsOpen = async () => {
+        selectedRows.forEach(async (row) => {
+            const event = row.original as Event;
+            await updateEventAreInscriptionsOpen(event.id, true);
+            row.toggleSelected();
+        });
+        await onActionCompleted();
+    };
+
+    const handleInscriptionsClose = async () => {
+        selectedRows.forEach(async (row) => {
+            const event = row.original as Event;
+            await updateEventAreInscriptionsOpen(event.id, false);
+            row.toggleSelected();
+        });
+        await onActionCompleted();
+    };
+
+    const handleToggleIsFinished = async () => {
+        selectedRows.forEach(async (row) => {
+            const event = row.original as Event;
+            await updateEventIsFinished(event.id);
+            row.toggleSelected();
+        });
+        await onActionCompleted();
+    };
+
     return (
         <div className="ml-20 flex gap-2">
             <TooltipProvider>
                 <Tooltip>
                     <TooltipTrigger asChild>
-                        <Button size={"icon"}>
+                        <Button size={"icon"} onClick={handleInscriptionsOpen}>
                             <Pencil />
                         </Button>
                     </TooltipTrigger>
@@ -21,7 +64,15 @@ export function ActionButtons() {
                 </Tooltip>
                 <Tooltip>
                     <TooltipTrigger asChild>
-                        <Button size={"icon"}>
+                        <Button size={"icon"} onClick={handleInscriptionsClose}>
+                            <PencilOff />
+                        </Button>
+                    </TooltipTrigger>
+                    <TooltipContent>Fechar Inscrições</TooltipContent>
+                </Tooltip>
+                <Tooltip>
+                    <TooltipTrigger asChild>
+                        <Button size={"icon"} onClick={handleToggleIsFinished}>
                             <Delete />
                         </Button>
                     </TooltipTrigger>
