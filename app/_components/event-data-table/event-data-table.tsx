588565c6a67aeddb9cf198a179e358594e07bc18
Jhonatan Cassante de Brito <jhonatan.cassante@live.com>
2025-02-25 15:54:46 -0300

    feat: placed the data-table in tabs

--- app/_components/event-data-table/event-data-table.tsx
+++ app/_components/event-data-table/event-data-table.tsx
@@ -3,6 +3,7 @@ import { DataTable } from "../data-table/data-table";
 import { eventColumns } from "./event-columns";
 import { DataTableEventActionButtons } from "./event-action-buttons";
 import { DataTableEventFinishedActionButtons } from "./event-finished-action-buttons";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "../ui/tabs";
 
 const EventDataTable = () => {
     const {
@@ -21,7 +22,51 @@ const EventDataTable = () => {
     return (
         <main className="flex flex-col items-center py-6">
             <h1 className="text-2xl font-bold">Lista de Eventos</h1>
-            <div className="mx-auto w-full py-6 md:max-w-[90%]">
+            <Tabs defaultValue="notFinished" className="mt-6 w-[90%]">
+                <TabsList className="grid w-full grid-cols-2">
+                    <TabsTrigger value="notFinished">
+                        Não Finalizados
+                    </TabsTrigger>
+                    <TabsTrigger value="finished">Finalizados</TabsTrigger>
+                </TabsList>
+                <TabsContent
+                    value="notFinished"
+                    className="mx-auto w-full py-3"
+                >
+                    <DataTable
+                        columns={eventColumns}
+                        itemList={eventListNotFinished}
+                        isLoading={isLoading}
+                        columnsWithFilters={columnsWithFilters}
+                        refreshList={refreshEvents}
+                        actionButtons={
+                            <DataTableEventActionButtons
+                                selectedRows={[]}
+                                onActionCompleted={refreshEvents}
+                            />
+                        }
+                    />
+                </TabsContent>
+                <TabsContent
+                    value="finished"
+                    className="mx-auto w-full py-3 md:max-w-[90%]"
+                >
+                    <DataTable
+                        columns={eventColumns}
+                        itemList={eventListFinished}
+                        isLoading={isLoading}
+                        columnsWithFilters={columnsWithFilters}
+                        refreshList={refreshEvents}
+                        actionButtons={
+                            <DataTableEventFinishedActionButtons
+                                selectedRows={[]}
+                                onActionCompleted={refreshEvents}
+                            />
+                        }
+                    />
+                </TabsContent>
+            </Tabs>
+            {/* <div className="mx-auto w-full py-3 md:max-w-[90%]">
                 <DataTable
                     columns={eventColumns}
                     itemList={eventListNotFinished}
@@ -35,11 +80,11 @@ const EventDataTable = () => {
                         />
                     }
                 />
-            </div>
-            <h1 className="mt-10 text-2xl font-bold">
+            </div> */}
+            {/* <h1 className="mt-10 text-2xl font-bold">
                 Lista de Eventos Finalizados
             </h1>
-            <div className="mx-auto w-full py-6 md:max-w-[90%]">
+            <div className="mx-auto w-full py-3 md:max-w-[90%]">
                 <DataTable
                     columns={eventColumns}
                     itemList={eventListFinished}
@@ -53,7 +98,7 @@ const EventDataTable = () => {
                         />
                     }
                 />
-            </div>
+            </div> */}
         </main>
     );
 };
