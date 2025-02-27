4e3adb14c23f937506dae9f053fbec14e0499bec
Jhonatan Cassante de Brito <jhonatan.cassante@live.com>
2025-02-26 23:18:23 -0300

    feat: add event creation button

--- app/_components/event-data-table/event-action-buttons.tsx
+++ app/_components/event-data-table/event-action-buttons.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import { Row } from "@tanstack/react-table";
-import { Pencil, PencilOff, Trash2, Delete } from "lucide-react";
+import { Pencil, PencilOff, Trash2, Delete, Plus } from "lucide-react";
 import { TooltipProvider } from "../ui/tooltip";
 import { ActionButton } from "../ui/action-button";
 import {
@@ -13,6 +13,8 @@ import { deleteEvent } from "@/app/_actions/deleteEvent";
 import { handleEventAction } from "@/app/_lib/event-actions";
 import DeleteDialog from "../delete-dialog";
 import { useLoading } from "@/app/_contexts/LoadingContext";
+import { useRouter } from "next/navigation";
+import { PATHS } from "@/app/_constants/paths";
 
 interface DataTableEventActionButtonsProps<TData> {
     selectedRows: Row<TData>[];
@@ -25,6 +27,7 @@ export const DataTableEventActionButtons = <TData,>({
 }: Readonly<DataTableEventActionButtonsProps<TData>>) => {
     const [deleteDialogOpen, setDeleteDialogOpen] = useState(false);
     const { setIsLoading } = useLoading();
+    const router = useRouter();
 
     const handleDialogOpen = () => {
         if (selectedRows.length === 0) return;
@@ -43,6 +46,11 @@ export const DataTableEventActionButtons = <TData,>({
     return (
         <div className="ml-20 flex gap-2">
             <TooltipProvider>
+                <ActionButton
+                    icon={<Plus />}
+                    tooltipText="Criar Novo Evento"
+                    onClick={() => router.push(PATHS.EVENT_ADD)}
+                />
                 <ActionButton
                     icon={<Pencil />}
                     tooltipText="Abrir Inscrições"
