6bbff6d94f12e814ea3e1321b52f87a751ac8352
Jhonatan Cassante de Brito <jhonatan.cassante@live.com>
2025-03-03 22:13:12 -0300

    style: change colors in loading indicator

--- app/_components/loading-indicator.tsx
+++ app/_components/loading-indicator.tsx
@@ -15,7 +15,7 @@ const LoadingIndicator: React.FC = () => {
             ></div>
             <div className="fixed left-0 top-0 z-20 flex h-[110vh] w-full flex-col items-center justify-center gap-4">
                 <div className="loader h-24 w-24 animate-spin rounded-full border-4 border-t-4 border-dotted border-background border-r-foreground border-t-foreground"></div>
-                <div className="text-xl font-bold text-foreground">
+                <div className="text-xl font-bold text-secondary drop-shadow-[0_0_4px_hsl(var(--primary))]">
                     Carregando...
                 </div>
             </div>
