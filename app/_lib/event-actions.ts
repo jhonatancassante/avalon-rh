eb8adae193a4aee0c63a93b4fbab852f878cef7a
Jhonatan Cassante de Brito <jhonatan.cassante@live.com>
2025-03-04 09:23:23 -0300

    fix: error occurred in the event action with the typing of the function return

--- app/_lib/event-actions.ts
+++ app/_lib/event-actions.ts
@@ -3,19 +3,22 @@ import { Event } from "@prisma/client";
 
 export const handleEventAction = async <TData>(
     rows: Row<TData>[],
-    action: (eventId: string) => Promise<void>,
+    action: (eventId: string) => Promise<Event | void>,
     onCompleted: () => Promise<void>,
     setIsLoading: (arg0: boolean) => void,
 ) => {
     if (rows.length === 0) return;
     setIsLoading(true);
-    await Promise.all(
-        rows.map(async (row) => {
-            const event = row.original as Event;
-            await action(event.id);
-            row.toggleSelected();
-        }),
-    );
-    await onCompleted();
-    setIsLoading(false);
+    try {
+        await Promise.all(
+            rows.map(async (row) => {
+                const event = row.original as Event;
+                await action(event.id);
+                row.toggleSelected();
+            }),
+        );
+        await onCompleted();
+    } finally {
+        setIsLoading(false);
+    }
 };
