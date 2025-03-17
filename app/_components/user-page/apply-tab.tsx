ddb7eb4c8048549f0ac4bc2b29a5d4daad4cdaa0
Jhonatan Cassante de Brito <jhonatan.cassante@live.com>
2025-03-17 20:10:21 -0300

    refactor: enhance ApplyTab component with dynamic tab count and loading state

--- app/_components/user-page/apply-tab.tsx
+++ app/_components/user-page/apply-tab.tsx
@@ -1,49 +1,67 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "../ui/tabs";
 import { Configs } from "@prisma/client";
 import { getConfigList } from "@/app/_data/getConfigs";
+import { useLoading } from "@/app/_contexts/LoadingContext";
 
 const ApplyTab = () => {
     const [configList, setConfigList] = useState<Configs[]>([]);
-    const [presenterApply, photographerApply] = [
-        "presenterApply",
-        "photographerApply",
-    ].map((configKey) => {
-        return configList.find((c) => c.key === configKey);
-    });
+    const [activeTabsCount, setActiveTabsCount] = useState<number>(1);
+    const { setIsLoading } = useLoading();
 
-    const fetchConfigs = async () => {
+    const gridColumns = {
+        1: "grid-cols-1",
+        2: "grid-cols-2",
+        3: "grid-cols-3",
+    };
+
+    const fetchConfigs = useCallback(async () => {
+        setIsLoading(true);
         const configs = await getConfigList();
         setConfigList(configs);
-    };
+        setActiveTabsCount(
+            1 +
+                [
+                    configs.find((c) => c.key === "presenterApply")?.value,
+                    configs.find((c) => c.key === "photographerApply")?.value,
+                ].filter(Boolean).length,
+        );
+        setIsLoading(false);
+    }, [setIsLoading]);
 
     useEffect(() => {
         fetchConfigs();
-    }, []);
+    }, [fetchConfigs]);
 
     return (
         <div className="flex w-full flex-col items-center p-0 pb-4">
-            <Tabs defaultValue="staff" className="sm:p- w-full">
-                <TabsList className="grid w-full grid-cols-3">
+            <Tabs defaultValue="staff" className="w-full">
+                <TabsList
+                    className={`grid w-full ${gridColumns[activeTabsCount as keyof typeof gridColumns]}`}
+                >
                     <TabsTrigger value="staff">Staff</TabsTrigger>
-                    {presenterApply?.value && (
+                    {configList.find((c) => c.key === "presenterApply")
+                        ?.value && (
                         <TabsTrigger value="presenter">
                             Apresentador
                         </TabsTrigger>
                     )}
-                    {photographerApply?.value && (
+                    {configList.find((c) => c.key === "photographerApply")
+                        ?.value && (
                         <TabsTrigger value="photographer">
                             Fotógrafo
                         </TabsTrigger>
                     )}
                 </TabsList>
+
                 <TabsContent value="staff">Staff</TabsContent>
-                {presenterApply?.value && (
+                {configList.find((c) => c.key === "presenterApply")?.value && (
                     <TabsContent value="presenter">Apresentador</TabsContent>
                 )}
-                {photographerApply?.value && (
+                {configList.find((c) => c.key === "photographerApply")
+                    ?.value && (
                     <TabsContent value="photographer">Fotógrafo</TabsContent>
                 )}
             </Tabs>
